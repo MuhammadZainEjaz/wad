b34f79498ed091d0faf1730f5c49c3d4a4c9eec4
MuhammadZainEjaz <40689374+MuhammadZainEjaz@users.noreply.github.com>
2019-01-05 23:51:04 +0500

    Add files via upload

--- pro/server/functions.php
+++ pro/server/functions.php
@@ -18,8 +18,8 @@
 
 	function get_categories($option){
 		global $con;
-		$get_brands_query = "select * from categories";
-		$result = mysqli_query($con, $get_brands_query);
+		$get_categories_query = "select * from categories";
+		$result = mysqli_query($con, $get_categories_query);
 		while($row = mysqli_fetch_assoc($result)){
 			$title = $row['cat_title'];
 			$id = $row['cat_id'];
@@ -31,3 +31,27 @@
             }
 		}
 	}
+
+    function show_data_on_content_page(){
+        global $con;
+        $get_products_query = "select * from products";
+        $result = mysqli_query($con, $get_products_query);
+        while($row = mysqli_fetch_assoc($result)){
+            $title = $row['pro_title'];
+            $query = $row['pro_cat'];
+            $result1 = mysqli_query($con, "select * from categories where cat_id = $query");
+            $pro_cat_id = mysqli_fetch_assoc($result1)['cat_title'];
+            $query = $row['pro_brand'];
+            $result1 = mysqli_query($con, "select * from brands where brand_id = $query");
+            $pro_brand_id = mysqli_fetch_assoc($result1)['brand_title'];
+            $price = $row['pro_price'];
+            $pro_desc = $row['pro_desc'];
+            $pro_keywords = $row['pro_keywords'];
+            echo "$title </br>";
+            echo "$pro_cat_id</br>";
+            echo "$pro_brand_id </br>";
+            echo "$price </br>";
+            echo "$pro_desc </br>";
+            echo "$pro_keywords </br> </br>";
+        }
+    }
