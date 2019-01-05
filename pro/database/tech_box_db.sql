609f4c96ba62d0e7f4e27a185fd252d065562d8a
MuhammadZainEjaz <40689374+MuhammadZainEjaz@users.noreply.github.com>
2019-01-05 23:54:00 +0500

    Add files via upload

--- pro/database/tech_box_db.sql
+++ pro/database/tech_box_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2019 at 05:55 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Jan 05, 2019 at 02:52 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -17,6 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `tech_box_db`
 --
@@ -29,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `brands` (
   `brand_id` int(100) NOT NULL,
-  `brand_title` varchar(255) NOT NULL
+  `brand_title` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -39,11 +40,9 @@ CREATE TABLE `brands` (
 INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
 (1, 'Apple'),
 (2, 'Dell'),
-(3, 'Samsung'),
-(4, 'Toshiba'),
-(5, 'HP'),
-(6, 'Oppo'),
-(7, 'Sony');
+(3, 'Hp'),
+(4, 'Oppo'),
+(5, 'Nokia');
 
 -- --------------------------------------------------------
 
@@ -52,8 +51,8 @@ INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
 --
 
 CREATE TABLE `categories` (
-  `cat_id` int(100) NOT NULL,
-  `cat_title` varchar(255) NOT NULL
+  `cat_id` int(10) NOT NULL,
+  `cat_title` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -61,13 +60,37 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
-(1, 'Laptops'),
-(2, 'Computers'),
-(5, 'Tablets'),
-(6, 'Watches'),
-(7, 'Cameras'),
-(8, 'iPads'),
-(9, 'Projectors');
+(1, 'Mobile'),
+(2, 'Phone'),
+(3, 'AC'),
+(4, 'Tablets'),
+(5, 'Cooler');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pro_id` int(10) NOT NULL,
+  `pro_title` varchar(300) NOT NULL,
+  `pro_cat` int(10) NOT NULL,
+  `pro_brand` int(10) NOT NULL,
+  `pro_price` int(100) NOT NULL,
+  `pro_image` varchar(300) NOT NULL,
+  `pro_desc` varchar(5000) NOT NULL,
+  `pro_keywords` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pro_id`, `pro_title`, `pro_cat`, `pro_brand`, `pro_price`, `pro_image`, `pro_desc`, `pro_keywords`) VALUES
+(1, 'Infinix', 1, 1, 1, '', 'sdsdsds', 'sasta, wella'),
+(2, 'Infinix', 1, 4, 4, '', 'sdsdsff', 'sasta, wella'),
+(3, 'Soonny', 3, 2, 120330, '', 'sdsds', 'sasta, wella');
 
 --
 -- Indexes for dumped tables
@@ -86,6 +109,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pro_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,14 +122,17 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
+  MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
+  MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
